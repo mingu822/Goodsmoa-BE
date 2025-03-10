9db09555a441f6944b517df586ab4971dc83ee3f
Jinseong0813 <wlstl258@gmail.com>
2025-03-10 19:18:09 +0900

    1
    1

--- src/main/java/com/goodsmoa/goodsmoa_BE/trade/Entity/TradePost.java
+++ src/main/java/com/goodsmoa/goodsmoa_BE/trade/Entity/TradePost.java
@@ -1,5 +1,6 @@
 package com.goodsmoa.goodsmoa_BE.trade.Entity;
 
+import com.goodsmoa.goodsmoa_BE.category.Entity.Category;
 import com.goodsmoa.goodsmoa_BE.user.Entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
