e8444c8b7613637bbf5175acb215ec9eab878f74
youngseo1124 <youngseo1124@example.com>
2025-04-11 11:59:34 +0900

    d
    d

--- src/main/java/com/goodsmoa/goodsmoa_BE/demand/repository/DemandOrderProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.goodsmoa.goodsmoa_BE.demand.repository;
-
-import com.goodsmoa.goodsmoa_BE.demand.entity.DemandOrderProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DemandOrderProductRepository extends JpaRepository<DemandOrderProductEntity, Long> {
-
-}
