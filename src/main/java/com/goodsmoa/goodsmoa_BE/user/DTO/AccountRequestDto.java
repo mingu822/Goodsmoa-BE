84a5e53cee0c85fde4c6d555b0eec63df1166ce3
youngseo1124 <youngseo1124@example.com>
2025-04-15 15:49:19 +0900

    Reapply "Merge branch 'yyyyyy'"
    This reverts commit 121be0e75f269f2805068436d31efcc35a5ab3ee.

---
+++ src/main/java/com/goodsmoa/goodsmoa_BE/user/DTO/AccountRequestDto.java
@@ -0,0 +1,12 @@
+package com.goodsmoa.goodsmoa_BE.user.DTO;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class AccountRequestDto {
+    private String accountName; // 은행명
+    private String name;        // 예금주명
+    private String number;      // 계좌번호
+}
