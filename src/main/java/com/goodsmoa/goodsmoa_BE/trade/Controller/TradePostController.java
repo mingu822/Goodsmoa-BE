fce6bfd3265953146e7b088d04505347257d32cd
Jinseong0813 <wlstl258@gmail.com>
2025-05-13 14:34:30 +0900

    1234
    1234

--- src/main/java/com/goodsmoa/goodsmoa_BE/trade/Controller/TradePostController.java
+++ src/main/java/com/goodsmoa/goodsmoa_BE/trade/Controller/TradePostController.java
@@ -9,6 +9,10 @@ import com.goodsmoa.goodsmoa_BE.trade.Service.TradePostService;
 import com.goodsmoa.goodsmoa_BE.user.Entity.UserEntity;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -89,5 +93,13 @@ public class TradePostController {
     public ResponseEntity<TradePostPulledResponse> pullTradePost(@PathVariable Long id) {
         return tradePostService.pullPost(id);
     }
+
+    //중고거래 리스트 조회
+    @GetMapping("/post")
+    public ResponseEntity<Page<TradePostLookResponse>> getTradePostList(
+            @PageableDefault(size = 10 , sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable){
+        return tradePostService.getTradePostList(pageable);
+    }
+
 }
 
---
+++ src/main/java/com/goodsmoa/goodsmoa_BE/trade/DTO/Post/TradePostLookResponse.java
@@ -0,0 +1,33 @@
+package com.goodsmoa.goodsmoa_BE.trade.DTO.Post;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TradePostLookResponse {
+
+    private Long id;  // ✅ ProductPost 의 PK (게시글 ID)
+
+    private String title;
+
+    private LocalDateTime createdAt;
+
+    private String thumbnailImage;
+
+    private Long views;
+
+    private String hashtag;
+
+    private String userId;
+
+    private String userNickName;
+
+    private String userImage;
+}
