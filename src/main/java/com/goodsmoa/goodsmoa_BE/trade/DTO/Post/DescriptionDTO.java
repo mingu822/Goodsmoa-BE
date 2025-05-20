d1f88bdb73f83add73a24cbeb72dde2d3d50ffa3
Jinseong0813 <wlstl258@gmail.com>
2025-05-21 02:17:38 +0900

    중고거래 오타 및 수정해야하는 에너테이션 추가
    jsonigonre라는 에너테이션들을 다른 곳들에 추가하였음 -> response 가 필요없는 값까지 다 응답해버려서 무시하는 걸로 하였음

--- src/main/java/com/goodsmoa/goodsmoa_BE/trade/DTO/Post/DescriptionDTO.java
+++ src/main/java/com/goodsmoa/goodsmoa_BE/trade/DTO/Post/DescriptionDTO.java
@@ -1,5 +1,7 @@
 package com.goodsmoa.goodsmoa_BE.trade.DTO.Post;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.goodsmoa.goodsmoa_BE.trade.Entity.TradePostDescription;
 import lombok.*;
 
@@ -8,6 +10,7 @@ import lombok.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)  //
 public class DescriptionDTO {
     private TradePostDescription.contentType type;
 
@@ -20,4 +23,20 @@ public class DescriptionDTO {
     private TradePostDescription.TextAlignment textAlignment;
 
     private String fontSize;
+
+    @JsonIgnore
+    public boolean isText(){
+        return "TEXT".equalsIgnoreCase(String.valueOf(type));
+    }
+
+    public void sanitize() {
+        if (isText()) {
+            if (this.textStyle == null) this.textStyle = TradePostDescription.TextStyle.NORMAL;
+            if (this.textAlignment == null) this.textAlignment = TradePostDescription.TextAlignment.LEFT;
+        } else {
+            this.textStyle = null;
+            this.textAlignment = null;
+            this.fontSize = null;
+        }
+    }
 }
